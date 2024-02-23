48ed86f337481205f507cb1e63e627f60143ac6d
MaxLeywyn <maxa3113@mail.ru>
2024-02-23 21:01:43 +0300

    init vectorVoid

---
+++ libs/data_structures/vector_void/vector_void.h
@@ -0,0 +1,28 @@
+#ifndef TEST_VECTOR_VOID_H
+#define TEST_VECTOR_VOID_H
+
+#include <limits.h>
+
+typedef struct vectorVoid {
+    void *data; // указатель на нулевой элемент вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+    size_t baseTypeSize; // размер базового типа:
+    // например, если вектор хранит int -
+    // то поле baseTypeSize = sizeof(int)
+    // если вектор хранит float -
+    // то поле baseTypeSize = sizeof(float)
+} vectorVoid;
+
+
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+void shrinkToFitV(vectorVoid *v);
+
+void clearV(vectorVoid *v);
+
+void deleteVectorV(vectorVoid *v);
+
+#endif //TEST_VECTOR_VOID_H
